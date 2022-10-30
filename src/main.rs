f4ef93718ebf6e1b59048122ab37b52cdea3dcea
Speretta <speretta@protonmail.com>
2022-10-30 16:38:43 +0300

    Better Byte Text Display

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn main(){
 
     }
 
-    println!("You have saved a total of {} bytes", style(all_size).green());
+    println!("You have saved a total of {}", style(bytes_to_string(all_size)).green());
    
     let mut buffer = String::new();
     std::io::stdin().read_line(&mut buffer).expect("Crashed while reading line");
@@ -75,4 +75,46 @@ fn find_projects(path: PathBuf, projects: &mut Vec<Project>){
         }
     }
 
+}
+
+const BYTEUNITS: [(usize, &str); 4] = [(1073741824, "gigabyte"), (1048576, "megabyte"), (1024, "kilobyte"), (1, "byte")];
+
+
+//Could have been a better algorithm
+fn bytes_to_string(mut bytes: usize) -> String{
+    let mut converted_text = String::new();
+    if bytes != 0{
+        let mut converted_text_vec = Vec::new();
+        for (unit_size, unit_text) in BYTEUNITS{
+            let count = bytes / unit_size ;
+            if count >= 1{
+                bytes -= count * unit_size;
+                let text = format!("{} {}{}", count, unit_text, (count != 1).then_some('s').unwrap_or('\0'));
+                converted_text_vec.push(text);
+            }
+        }
+        let converted_text_vec_len = converted_text_vec.len();
+        if converted_text_vec_len > 1{
+            for (i, text) in converted_text_vec.into_iter().enumerate(){
+                if i != converted_text_vec_len-1{
+                    converted_text.push_str(text.as_str());
+                    if i != converted_text_vec_len-2{
+                        converted_text.push_str(", ");
+                    }else{
+                        converted_text.push_str(" and ");
+                    }
+                    
+                }else{
+                    converted_text.push_str(text.as_str());
+                }
+            }
+        }
+        
+        
+    }else{
+        converted_text.push_str("0 byte");
+    }
+    
+
+    converted_text
 }
